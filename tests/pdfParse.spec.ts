af5f57a4a2b4653eb6475fe93419d5b10710cf10
AndriiCar <kravchenko.svg@gmail.com>
2024-07-29 16:18:22 +0300

    Create pdf parse tests

---
+++ tests/pdfParse.spec.ts
@@ -0,0 +1,31 @@
+import { expect, test } from '@playwright/test';
+import { ACTUAL_PDF, EXPECTER_PDF } from '../constants/constants';
+import fs from 'fs';
+import path from 'path';
+import { DATA_FOLDER } from '../playwright.config';
+import pdf from 'pdf-parse';
+
+const basePdfText = (date: string) => `
+
+${date} 
+PDF testing `;
+
+test.describe('pdf-parse', { tag: '@pdf-parse' }, async () => {
+    test('should compare identical PDFs', async ({}) => {
+        const pdfData = await pdf(
+            fs.readFileSync(path.join(DATA_FOLDER, EXPECTER_PDF)),
+        );
+
+        expect(pdfData.numpages).toEqual(1);
+        expect(pdfData.text).toEqual(basePdfText('01.01.2000'));
+    });
+
+    test.only('should compare different PDFs', async ({}) => {
+        const pdfData = await pdf(
+            fs.readFileSync(path.join(DATA_FOLDER, ACTUAL_PDF)),
+        );
+
+        expect(pdfData.numpages).toEqual(1);
+        expect(pdfData.text).toEqual(basePdfText('01.01.2024'));
+    });
+});
